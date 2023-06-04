008c07c02a77c5294470f0719074e8cfbe06d4dc
andrewberning <berningan89@gmail.com>
2023-06-04 13:48:44 -0700

    Revert "First Commit"
    This reverts commit 64e5e41fcc7a18f446c1c5922a689ebd49628a72.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,44 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-import { setSearchField, requestRobots } from '../actions';
-
-const mapStateToProps = (state) => {
-  return {
-    searchField: state.searchRobots.searchField,
-    isPending: state.requestRobots.isPending,
-    robots: state.requestRobots.robots,
-    error: state.requestRobots.error
-  }
-}
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
-
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
   componentDidMount() {
-    this.props.onRequestRobots();
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      })
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
   }
 
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
 
-    return isPending ?
+    return !robots.length ?
       <h1 className="f1 tc">Loading...</h1> :
       (
         <div className="tc">
           <h1 className="f1">Robo Amigos</h1>
-          <Searchbox searchChange={onSearchChange} />
+          <Searchbox searchChange={this.onSearchChange} />
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -47,4 +47,4 @@ class App extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
-
-const logger = createLogger();
-const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware)); // add logger after thunkMiddlewar for logs
 
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
-  <Provider store={store}>
+  <StrictMode>
     <App />
-  </Provider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
